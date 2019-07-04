19043be74c06f32fbc1ddb3d368b4d158cdfcdfb
alexandre barreto <alexandrebdesouza1@gmail.com>
2019-07-04 18:27:51 -0300

    Adiçao do crud

---
+++ src/app/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  id: number;
+  marca: string;
+  modelo: string;
+  anoModelo: number;
+  cor: string;
+  placa:string;
+  km:string;
+  combustivel:string;
+  valor:string;
+  tipoVeiculo:string;
+}
